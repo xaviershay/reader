f26db2c685cc14dd0e1a81ae8cf54ebb825f6369
Xavier Shay <xavier@rhnh.net>
2009-04-10 10:56:48 +1000

    refresh.sh script, to be cronned

---
+++ bin/refresh.sh
@@ -0,0 +1,5 @@
+/usr/bin/env python /opt/apps/reader/planet.py /opt/apps/reader/planets/humour.ini
+/usr/bin/env python /opt/apps/reader/planet.py /opt/apps/reader/planets/life.ini
+/usr/bin/env python /opt/apps/reader/planet.py /opt/apps/reader/planets/tech.ini
+/usr/bin/env python /opt/apps/reader/planet.py /opt/apps/reader/planets/byteclub.ini
+/usr/bin/env python /opt/apps/reader/planet.py /opt/apps/reader/planets/htfu.ini
