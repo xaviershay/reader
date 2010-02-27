42e31a8be45d338b583d44d568b6ad2b16f1093e
Xavier Shay <xavier@rhnh.net>
2010-02-27 17:33:44 +1100

    Remove some old stuff

--- bin/refresh.sh
+++ bin/refresh.sh
@@ -1,5 +1,3 @@
 /usr/bin/env python /opt/apps/reader/current/planet.py /opt/apps/reader/current/planets/humour.ini
 /usr/bin/env python /opt/apps/reader/current/planet.py /opt/apps/reader/current/planets/life.ini
 /usr/bin/env python /opt/apps/reader/current/planet.py /opt/apps/reader/current/planets/tech.ini
-/usr/bin/env python /opt/apps/reader/current/planet.py /opt/apps/reader/current/planets/byteclub.ini
-/usr/bin/env python /opt/apps/reader/current/planet.py /opt/apps/reader/current/planets/htfu.ini
